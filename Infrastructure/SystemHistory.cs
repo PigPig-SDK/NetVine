2753f5c55873f6c5244b93f5e2684517b28a95c0
Sean H <DjPigPigstar@gmail.com>
2026-02-19 10:57:42 -0800

    Removed debug

--- Infrastructure/SystemHistory.cs
+++ Infrastructure/SystemHistory.cs
@@ -41,7 +41,6 @@ public class SystemHistory : IDisposable
         //Incase context switch or lag causes the timer to be delayed.
         TimeSpan elapsed = _stopwatch.Elapsed;
         _stopwatch.Restart();
-        Console.WriteLine($"Taking snapshot at {DateTime.Now} with elapsed time {elapsed.Seconds}");
 
         try
         {
