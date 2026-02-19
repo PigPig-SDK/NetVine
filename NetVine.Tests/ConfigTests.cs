0577a402d6a67bd825c34d80525f5058b2a18a48
benFoster1234567 <156257805+benFoster1234567@users.noreply.github.com>
2026-02-18 20:52:35 -0800

    Added Test Project For ConfigManager

---
+++ NetVine.Tests/ConfigTests.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure;
+
+namespace NetVine.Tests;
+
+/// <summary>
+/// Todo: Implement tests for the ConfigManager
+/// </summary>
+public class ConfigTests
+{
+    [Fact]
+    public void ReadFromFile_CreatesDefaultSettings_WhenFileNotFound()
+    {
+        var config = ConfigManager.Instance;
+        bool result = config.ReadFromFile();
+
+        Assert.False(result);
+        Assert.NotNull(config.ConfigSettings);
+        Assert.True(config.ConfigSettings.TrackCPUUsage);
+    }
+}
