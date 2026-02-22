094c731ec5622427cd32748173ce4ca4477a2f4f
Sean H <DjPigPigstar@gmail.com>
2026-02-22 14:15:06 -0800

    Formatting to code + Event listener

--- Infrastructure/ConfigManager.cs
+++ Infrastructure/ConfigManager.cs
@@ -7,6 +7,19 @@ namespace Infrastructure;
 
 public class ConfigManager
 {
+
+    private static ConfigManager? _Instance;
+
+    public static ConfigManager Instance
+    {
+        get
+        {
+            if (_Instance == null) _Instance = _Default;
+            return _Instance;
+        }
+        private set => _Instance = value;
+    }
+
     /// <summary>Dictionary containing default integer setting values.</summary>
     private static readonly Dictionary<SettingInt, int> _DefaultIntValues = new()
     {
@@ -48,7 +61,12 @@ public class ConfigManager
     [YamlMember]
     public Dictionary<SettingString, string> _StringSettings;
 
-
+    /// <summary>
+    /// Called when a setting is changed.
+    /// The parameter is the enum of the setting.
+    /// </summary>
+    /// <remarks>Expect Enum of : SettingInt, SettingFloat or SettingString</remarks>
+    public static Action<Enum>? OnSettingChanged { get; set; }
 
     /// <summary>
     /// Initializes a new instance of the ConfigManager class with default configuration file path and empty settings
@@ -94,10 +112,6 @@ public class ConfigManager
             _StringSettings[setting] = _DefaultStringValues.ContainsKey(setting) ? _DefaultStringValues[setting] : string.Empty;
     }
 
-
-
-
-
     /// <summary>
     /// Retrieves the value associated with the specified application setting.
     /// </summary>
@@ -105,9 +119,9 @@ public class ConfigManager
     /// setting exists in the configuration before calling this method.</remarks>
     /// <param name="setting">The setting for which to retrieve the integer value. Must be a valid member of the SettingInt enumeration.</param>
     /// <returns>The integer value corresponding to the specified setting.</returns>
-    public static int ReadSetting(SettingInt setting) { return Instance._IntSettings[setting]; }
-    public static float ReadSetting(SettingFloat setting) { return Instance._FloatSettings[setting]; }
-    public static string ReadSetting(SettingString setting) { return Instance._StringSettings[setting]; }
+    public static int ReadSetting(SettingInt setting) => Instance._IntSettings[setting];
+    public static float ReadSetting(SettingFloat setting) => Instance._FloatSettings[setting];
+    public static string ReadSetting(SettingString setting) => Instance._StringSettings[setting];
 
     /// <summary>
     /// Assigns the specified integer value to the given integer setting.
@@ -116,24 +130,29 @@ public class ConfigManager
     /// setting is valid before calling this method.</remarks>
     /// <param name="setting">The integer setting to update. Must be a valid value of the SettingInt enumeration.</param>
     /// <param name="value">The integer value to assign to the specified setting.</param>
-    public static void WriteSetting(SettingInt setting, int value) {  Instance._IntSettings[setting] = value; }
-    public static void WriteSetting(SettingFloat setting, float value) {  Instance._FloatSettings[setting] = value; }
-    public static void WriteSetting(SettingString setting, string value) {  Instance._StringSettings[setting] = value; }
-
-    /// <summary></summary>
-    private static ConfigManager? _Instance;
-
-    public static ConfigManager Instance
+    public static void WriteSetting(SettingInt setting, int value) 
     {
-        get
-        {
-            if (_Instance == null) _Instance = _Default;
-            return _Instance;
-        }
-        private set => _Instance = value;
+        Instance._IntSettings[setting] = value;
+        OnSettingChanged?.Invoke(setting);
     }
 
+    /// <summary>
+    /// Assigns the specified float value to the given float setting.
+    /// </summary>
+    public static void WriteSetting(SettingFloat setting, float value) 
+    {  
+        Instance._FloatSettings[setting] = value;
+        OnSettingChanged?.Invoke(setting);
+    }
 
+    /// <summary>
+    /// Assigns the specified string value to the given string setting.
+    /// </summary>
+    public static void WriteSetting(SettingString setting, string value) 
+    {  
+        Instance._StringSettings[setting] = value;
+        OnSettingChanged?.Invoke(setting);
+    }
 
     /// <summary>
     /// Gets the default instance of the ConfigManager class, which provides access to configuration settings.
@@ -164,7 +183,6 @@ public class ConfigManager
             return Path.Combine(folder, "config.yaml");
         }
     }
-
 
     /// <summary>
     /// Loads a configuration from a YAML file at the specified path.
@@ -271,7 +289,4 @@ public class ConfigManager
             emitter.Emit(new Scalar(value!.ToString()!));
         }
     }
-
-
-
 }
