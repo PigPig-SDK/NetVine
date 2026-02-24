968dd6f17e77e0785c08107ff312b00c63104158
Ty Greene <kidwhix@gmail.com>
2026-02-23 21:54:10 -0800

    Views/tabs
    Added canvas, folder view, and component view. Added tabs for historical and live.

---
+++ UI/Views/Canvas.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI;
+
+public partial class Canvas : UserControl
+{
+    public Canvas()
+    {
+        InitializeComponent();
+    }
+}
