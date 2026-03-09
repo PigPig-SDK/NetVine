895b6d542a6428deb95e15da7e82c4371d1327e8
Ty Greene <kidwhix@gmail.com>
2026-03-08 21:43:08 -0700

    Scottplot
    Added scottplot to project

--- UI/Views/Canvas.axaml.cs
+++ UI/Views/Canvas.axaml.cs
@@ -1,7 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-
+using ScottPlot;
 namespace UI;
 
 public partial class Canvas : UserControl
@@ -9,5 +9,9 @@ public partial class Canvas : UserControl
     public Canvas()
     {
         InitializeComponent();
+
+        double[] dataX = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        double[] dataY = new double[] { -1, 2, 3, -4, 5, 6, -7, 8, 9, -10, 11, 12, -13 };
+        
     }
 }
