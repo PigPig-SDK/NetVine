a8e24dcb272246e380a48fc0326449267a5ca068
Sean H <DjPigPigstar@gmail.com>
2026-03-09 21:33:16 -0700

    SelectedUsers

--- UI/ViewModels/FolderViewData.cs
+++ UI/ViewModels/FolderViewData.cs
@@ -7,6 +7,15 @@ namespace UI.ViewModels;
 public static class FolderViewData
 {
     public static Dictionary<string, FolderUser> UserMapping = [];
+
+    public static IEnumerable<string> SelectedUsers()
+    {
+        foreach (var user in UserMapping)
+        {
+            if (user.Value.IsSelected) yield return user.Key;
+        }
+    }
+
     public static Action<User>? OnUserAdded;
     public static Action? OnSelectionUpdated;
 }
