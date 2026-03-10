5b617f8d91db93e8ecf880b84c14bfb50cea489f
Ty Greene <kidwhix@gmail.com>
2026-03-09 21:17:18 -0700

    linegraph added

--- UI/Views/Canvas.axaml.cs
+++ UI/Views/Canvas.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using ScottPlot;
+using ScottPlot.Avalonia;
 namespace UI;
 
 public partial class Canvas : UserControl
@@ -10,8 +11,13 @@ public partial class Canvas : UserControl
     {
         InitializeComponent();
 
-        double[] dataX = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-        double[] dataY = new double[] { -1, 2, 3, -4, 5, 6, -7, 8, 9, -10, 11, 12, -13 };
-        
+        double[] dataX = new double[] {};
+        double[] dataY = new double[] {};
+
+        AvaPlot LineGraph = this.Find<AvaPlot>("LineGraph");
+        LineGraph.Plot.Add.Scatter(dataX, dataY);
+        LineGraph.Plot.Axes.Left.Label.Text = "Vertical Axis Label";
+        LineGraph.Plot.Axes.Bottom.Label.Text = "Horizontal Axis Label";
+        LineGraph.Refresh();
     }
 }
