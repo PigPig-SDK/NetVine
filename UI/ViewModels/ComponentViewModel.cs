34ec6c6f00d3c7d976f431a84d2db016cca62d58
Ty Greene <kidwhix@gmail.com>
2026-03-20 14:21:48 -0700

    button values
    added placeholder values for current cpu/gpu/ram/disk usage and changed formatting

--- UI/ViewModels/ComponentViewModel.cs
+++ UI/ViewModels/ComponentViewModel.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UI.ViewModels
 {
-    internal class ComponentViewModel
+    public class ComponentViewModel : INotifyPropertyChanged
     {
+        private string CPUtext = "0.0%";
+        private string GPUtext = "0.0%";
+        private string RAMtext = "0.0%";
+        private string DISKtext = "0.0%";
+
+        public string ButtonCPUText
+        {
+            get => CPUtext;
+            set
+            {
+                CPUtext = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonCPUText)));
+            }
+        }
+        public string ButtonGPUText
+        {
+            get => GPUtext;
+            set
+            {
+                GPUtext = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonGPUText)));
+            }
+        }
+        public string ButtonRAMText
+        {
+            get => RAMtext;
+            set
+            {
+                RAMtext = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonRAMText)));
+            }
+        }
+        public string ButtonDISKText
+        {
+            get => DISKtext;
+            set
+            {
+                DISKtext = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonDISKText)));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
