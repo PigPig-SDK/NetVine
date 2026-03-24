32687348f7136cdce2746cb1cec040e8bde04a92
benFoster1234567 <156257805+benFoster1234567@users.noreply.github.com>
2026-03-23 21:01:06 -0700

    Create DebugLogger.cs
    Created debuglogger to send debug info to a log file and/or the console.

---
+++ UI/ViewModels/DebugLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UI.ViewModels
+{
+    public static class DebugLogger
+    {
+        private static readonly DateTime _startTime = DateTime.Now;
+        private static readonly string _logFile = System.IO.Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory, "debug.txt");
+
+        public static bool ConsoleEnabled = false;
+        public static bool Enabled = false;
+
+        public static void Log(string message, string? fileName = null)
+        {
+            if (!Enabled) return;
+            var elapsed = DateTime.Now - _startTime;
+            var line = $"[{DateTime.Now:HH:mm:ss\\:ms}] [+{elapsed:hh\\:mm\\:ss\\:ms}] {message}";
+            if (ConsoleEnabled) Console.WriteLine(line);
+            var file = fileName ?? _logFile;
+            System.IO.File.AppendAllText(file, line + Environment.NewLine);
+        }
+
+    }
+}
