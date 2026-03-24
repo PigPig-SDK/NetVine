b3de9a542b40f9f15cec544e1b6ef465b3b0bd99
Sean H <DjPigPigstar@gmail.com>
2026-03-24 16:26:05 -0700

    Fixed namespace

--- Core/Debug.cs
+++ Core/Debug.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 
-namespace UI.ViewModels
+namespace Core;
+
+public static partial class Debug
 {
-    public static partial class Debug
+    private static readonly DateTime _startTime = DateTime.Now;
+    private static readonly string _logFile = System.IO.Path.Combine(
+        AppDomain.CurrentDomain.BaseDirectory, "debug.txt");
+
+    //enable for checking the table view 
+    public static bool ConsoleEnabled = true;
+    public static bool FileEnabled = false;
+    public static bool Enabled = true;
+
+    public static void Log(string message, string? fileName = null)
     {
-        private static readonly DateTime _startTime = DateTime.Now;
-        private static readonly string _logFile = System.IO.Path.Combine(
-            AppDomain.CurrentDomain.BaseDirectory, "debug.txt");
-
-        //enable for checking the table view 
-        public static bool ConsoleEnabled = true;
-        public static bool FileEnabled = false;
-        public static bool Enabled = true;
-
-        public static void Log(string message, string? fileName = null)
-        {
-            if (!Enabled) return;
-            var elapsed = DateTime.Now - _startTime;
-            var line = $"[{DateTime.Now:HH:mm:ss\\:ms}] [+{elapsed:hh\\:mm\\:ss\\:ms}] {message}";
-            if (ConsoleEnabled) Console.WriteLine(line);
-            var file = fileName ?? _logFile;
-            if (FileEnabled) System.IO.File.AppendAllText(file, line + Environment.NewLine);
-        }
-
+        if (!Enabled) return;
+        var elapsed = DateTime.Now - _startTime;
+        var line = $"[{DateTime.Now:HH:mm:ss\\:ms}] [+{elapsed:hh\\:mm\\:ss\\:ms}] {message}";
+        if (ConsoleEnabled) Console.WriteLine(line);
+        var file = fileName ?? _logFile;
+        if (FileEnabled) System.IO.File.AppendAllText(file, line + Environment.NewLine);
     }
+
 }
