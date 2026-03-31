c9ea61a53fee56830b0b1fc0ce0b0054141c9f39
Ty Greene <kidwhix@gmail.com>
2026-03-30 20:35:43 -0700

    Update Canvas.axaml.cs

--- UI/Views/Canvas.axaml.cs
+++ UI/Views/Canvas.axaml.cs
@@ -22,6 +22,8 @@ public partial class Canvas : UserControl
         _canvasPlot.Plot.DataBackground.Color = ScottPlot.Color.FromHex("#2D2D38");
         _canvasPlot.Plot.Axes.Color(ScottPlot.Color.FromHex("#CCCCCC"));
         _canvasPlot.Plot.Grid.MajorLineColor = ScottPlot.Color.FromHex("#FFFFFF").WithAlpha(0.1);
+
+        _canvasPlot.Plot.Axes.SetLimitsX(0, 100);
         _canvasPlot.Refresh();
     }
 
