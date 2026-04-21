ff3f6cc6d3b9c0ea6447006b4c8bff9f68fdc04f
Sean H <DjPigPigstar@gmail.com>
2026-04-20 22:13:07 -0700

    Moved quit command to individual file

--- CLI/Commands/ClearCommand.cs
+++ CLI/Commands/ClearCommand.cs
@@ -18,19 +18,3 @@ public class ClearCommand : Command
         App.PrintIntro();
     }
 }
-
-public class QuitCommand : Command
-{
-    public QuitCommand(App app) : base(app) { }
-
-    public override string Name => "quit";
-
-    public override string Description => "Closes the application.";
-
-    public override string Usage => "'quit'";
-
-    public override void Execute(string[] args)
-    {
-        App.IsRunning = false;
-    }
-}
