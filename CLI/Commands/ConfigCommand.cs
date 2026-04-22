41b15ad865096c0d958c17ba755ad99b2e7df086
Sean H <DjPigPigstar@gmail.com>
2026-04-22 14:17:57 -0700

    Linux configuration

--- CLI/Commands/ConfigCommand.cs
+++ CLI/Commands/ConfigCommand.cs
@@ -16,7 +16,17 @@ public class ConfigCommand : Command
     public override void Execute(string[] args)
     {
         string configLocation = ConfigManager.DefaultFilePath;
-        Process.Start(new ProcessStartInfo(configLocation) { UseShellExecute = true });
+        if (OperatingSystem.IsWindows())
+            Process.Start(new ProcessStartInfo(configLocation) { UseShellExecute = true });
+        else if (OperatingSystem.IsMacOS())///Untested. Fuck mac.
+            Process.Start("open", configLocation);
+        else if(OperatingSystem.IsLinux())
+        {
+            Console.Write("\x1b[?1049l"); // Exit alternate screen buffer
+            Process.Start("nano", configLocation)?.WaitForExit();
+            Console.Write("\x1b[?1049h"); // Re-enter alternate screen buffer
+        }
+
         App.SystemMessage($"Opened configuration file at '{configLocation}'");
     }
 }
