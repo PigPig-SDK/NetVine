154a4338f415322acff3092a6acba7cdbceea22e
ADeb51 <aidandebroeck@gmail.com>
2026-04-24 02:30:08 -0700

    HistoricalLineGraph Main

--- UI/ViewModels/ChartService.cs
+++ UI/ViewModels/ChartService.cs
@@ -6,9 +6,19 @@ public class ChartService
     public static readonly ChartService Instance = new();
 
     public const string CPU = "CPU";
+    public const string CPUAvg = "CPU Avg";
+    public const string CPUPeak = "CPU Peak";
     public const string RAM = "RAM";
+    public const string RAMAvg = "RAM Avg";
+    public const string RAMPeak = "RAM Peak";
     public const string DISK = "DISK";
+    public const string DISKAvg = "DISK Avg";
+    public const string DISKPeak = "DISK Peak";
     public const string NET = "NET";
+    public const string NETAvg = "NET Avg";
+    public const string NETPeak = "NET Peak";
+    public const string NETTotal = "NET Total";
+    
 
     public const string Line = "Line";
     public const string Bar = "Bar";
