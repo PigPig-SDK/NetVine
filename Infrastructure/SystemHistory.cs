17685d48ca5fdca18ac0e6f31e266a4a764a889e
Sean H <DjPigPigstar@gmail.com>
2026-04-27 19:35:50 -0700

    Removed linux producer

--- Infrastructure/SystemHistory.cs
+++ Infrastructure/SystemHistory.cs
@@ -43,10 +43,6 @@ public class SystemHistory : IDisposable
         {
             producer = new WindowsDataProducer();
         }
-        else if(OperatingSystem.IsLinux())
-        {
-            producer = new LinuxDataProducer();
-        }
 
         if (producer == null)
             throw new InvalidOperationException("No valid producer for your operating system exists!");
