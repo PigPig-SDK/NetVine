135de80f04edea9b7cb99b3cc6a7e13f6a13fe28
Ty Greene <kidwhix@gmail.com>
2026-05-04 23:13:09 -0700

    frontend draft 1
    log button added to sidebar with search and filter (not yet implemented)

---
+++ Infrastructure/LogEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class LogEntry
+    {
+        public string Timestamp { get; set; }
+        public string Level { get; set; }      // e.g. "INFO", "WARN", "ERROR"
+        public string Message { get; set; }
+        public string LevelColor => Level switch
+        {
+            "ERROR" => "#C88282",
+            "WARN" => "#FFFFFF",
+            "INFO" => "#A5c882",
+            _ => "#FFFFFF"
+        };
+    }
+}
