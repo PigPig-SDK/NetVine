e72c3e0cb881fd20c250dfd252836884923a2b9d
Sean H <DjPigPigstar@gmail.com>
2026-05-05 14:09:24 -0700

    CLI Messae commands

---
+++ CLI/Commands/MessageCommand.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Networking;
+using Infrastructure.Networking.Packets;
+using Infrastructure.Notifications;
+using Spectre.Console;
+
+namespace CLI.Commands;
+
+public class MessageCommand : Command
+{
+    public MessageCommand(App app) : base(app) { }
+
+    public override string Name => "msg";
+
+    public override string Description => "Sends a message to all immediate connections.";
+
+    public override string Usage => "'msg Text of any shape afterwards'";
+
+    public override void Execute(string[] args)
+    {
+        if(args.Length < 1)
+        {
+            App.PrintError(Name, Usage);
+            return;
+        }
+        string message = string.Join(" ", args);
+        NotificationManager.WriteNotification(
+            new Notification(NotificationPriority.Message, message));
+        var data = Packet.CreatePacket(new StringPayload(message)).ToBytes();
+        NetworkManager.Instance.SendToAll(data);
+        AnsiConsole.MarkupLineInterpolated($"Sent message.");
+    }
+}
