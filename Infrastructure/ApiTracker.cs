10f972a5fee8a80d7482ebac1a134ba54432c0ae
Sean H <DjPigPigstar@gmail.com>
2026-05-25 18:43:56 -0700

    API setup

---
+++ Infrastructure/ApiTracker.cs
@@ -0,0 +1,71 @@
+﻿using Core;
+using Infrastructure.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+
+public class ApiTracker
+{
+    public static ApiTracker Instance { get; private set; } = null!;
+    private HttpClient client = null!;
+
+    public ApiTracker()
+    {
+        client = new HttpClient();
+    }
+    ~ApiTracker()
+    {
+        client.Dispose();
+    }
+
+    public static void Setup()
+    {
+        Instance = new();
+        DBInteract.OnProgramListAdded += Instance.DbSnapshot;
+        SystemHistory.Instance.OnSnapshotTaken += Instance.Snapshot;
+        NotificationManager.OnNotified += Instance.OnNotified;
+    }
+
+    public static void Shutdown()
+    {
+        DBInteract.OnProgramListAdded -= Instance.DbSnapshot;
+        SystemHistory.Instance.OnSnapshotTaken -= Instance.Snapshot;
+        NotificationManager.OnNotified -= Instance.OnNotified;
+        Instance = null!;
+    }
+
+    private void DbSnapshot(List<ProgramData> programs, bool isDataLocal)
+    {
+        if (!GetUrlSetting(SettingString.ApiDataBaseSnapshot, out string? url)) return;
+        client.PostAsJsonAsync(url, (programs, isDataLocal));
+    }
+
+    private void OnNotified(Notification notification)
+    {
+        if (!GetUrlSetting(SettingString.ApiNotification, out string? url)) return;
+        client.PostAsJsonAsync(url, notification);
+    }
+
+    private void Snapshot(List<IProgramData> list)
+    {
+        if (!GetUrlSetting(SettingString.ApiSnapshot, out string? url)) return;
+        client.PostAsJsonAsync(url, list);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="setting"> The setting to evaluate</param>
+    /// <param name="url"> The users setting</param>
+    /// <returns>True if a URL setting exists</returns>
+    private bool GetUrlSetting(SettingString setting, out string? url)
+    {
+        url = ConfigManager.ReadSetting(setting);
+        return !string.IsNullOrEmpty(url);//Has valid value
+    }
+}
--- Infrastructure/Netvine.cs
+++ Infrastructure/Netvine.cs
@@ -34,6 +34,8 @@ public static class Netvine
 
         //Notification Tracker setup last. Timings are not important
         NotificationTracker.Setup();
+
+        ApiTracker.Setup();
     }
     public static void Shutdown()
     {
@@ -42,5 +44,7 @@ public static class Netvine
         NotificationManager.TrySaveToFile();
 
         NetworkManager.Instance.Disconnect();
+
+        ApiTracker.Shutdown();
     }
 }
