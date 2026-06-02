2299ea2732e21cb62767dab35f1ef6b66d0d7545
ADeb51 <aidandebroeck@gmail.com>
2026-06-01 17:46:45 -0700

    Reset DB Fix

--- Infrastructure/DBInteract.cs
+++ Infrastructure/DBInteract.cs
@@ -151,10 +151,13 @@ public class DBInteract : DbContext
     /// </summary>
     public static void ClearAllProgramData()
     {
-        using (var db = new DBInteract())
+        lock (DBLock)
         {
-            db.ProgramDataTable.RemoveRange(db.ProgramDataTable.ToList());
-            db.SaveChanges();
+            using (var db = new DBInteract())
+            {
+                db.ProgramDataTable.RemoveRange(db.ProgramDataTable.ToList());
+                db.SaveChanges();
+            }
         }
     }
     
@@ -163,10 +166,13 @@ public class DBInteract : DbContext
     /// </summary>
     public static void ClearAllProgramDataHistorical()
     {
-        using (var db = new DBInteract())
+        lock (DBLock)
         {
-            db.PDHTable.RemoveRange(db.PDHTable.ToList());
-            db.SaveChanges();
+            using (var db = new DBInteract())
+            {
+                db.PDHTable.RemoveRange(db.PDHTable.ToList());
+                db.SaveChanges();
+            }   
         }
     }
     
